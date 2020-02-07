9c8d809fff289c5c75baea6761eee4ac9f495d69
wangkun <wangkun@woodemi.com>
2020-02-07 08:56:30 +0800

    Impl notepad connect
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- windows/sample_plugin.cpp
+++ windows/sample_plugin.cpp
@@ -19,6 +19,7 @@
 #include <winrt/Windows.Foundation.Collections.h>
 #include <winrt/Windows.Devices.Bluetooth.h>
 #include <winrt/Windows.Devices.Bluetooth.Advertisement.h>
+#include <winrt/Windows.Devices.Bluetooth.GenericAttributeProfile.h>
 #include <winrt/Windows.Storage.Streams.h>
 
 #include <flutter/method_channel.h>
@@ -30,11 +31,19 @@
 #include <map>
 #include <memory>
 #include <string>
+#include <sstream>
+#include <iomanip>
 
 using namespace winrt;
+using namespace Windows::Foundation;
 using namespace Windows::Foundation::Collections;
+using namespace Windows::Devices::Bluetooth;
 using namespace Windows::Devices::Bluetooth::Advertisement;
+using namespace Windows::Devices::Bluetooth::GenericAttributeProfile;
 using namespace Windows::Storage::Streams;
+
+#define GUID_FORMAT "%08x-%04hx-%04hx-%02hhx%02hhx-%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx"
+#define GUID_ARG(guid) guid.Data1, guid.Data2, guid.Data3, guid.Data4[0], guid.Data4[1], guid.Data4[2], guid.Data4[3], guid.Data4[4], guid.Data4[5], guid.Data4[6], guid.Data4[7]
 
 namespace {
     using flutter::EncodableMap;
@@ -44,6 +53,38 @@ namespace {
         uint16_t uint16;
         byte bytes[sizeof(uint16_t)];
     };
+
+    std::vector<uint8_t> to_bytevc(IBuffer buffer)
+    {
+        //auto rangeFirst = buffer.data();
+        //return std::vector<uint8_t>{ rangeFirst, rangeFirst + buffer.Length() };
+        auto reader = DataReader::FromBuffer(buffer);
+        auto result = std::vector<uint8_t>(reader.UnconsumedBufferLength());
+        reader.ReadBytes(result);
+        return result;
+    }
+
+    IBuffer from_bytevc(std::vector<uint8_t> bytes)
+    {
+        auto writer = DataWriter();
+        writer.WriteBytes(bytes);
+        return writer.DetachBuffer();
+    }
+
+    std::string to_hexstring(std::vector<uint8_t> bytes)
+    {
+        auto ss = std::stringstream();
+        for (auto b : bytes)
+            ss << std::setw(2) << std::setfill('0') << std::hex << static_cast<int>(b);
+        return ss.str();
+    }
+    
+    std::string to_uuidstr(guid guid)
+    {
+        char chars[36 + 1];
+        sprintf_s(chars, GUID_FORMAT, GUID_ARG(guid));
+        return std::string{ chars };
+    }
 
     // *** Rename this class to match the linux pluginClass in your pubspec.yaml.
     class SamplePlugin : public flutter::Plugin
@@ -61,11 +102,29 @@ namespace {
             const flutter::MethodCall<EncodableValue>& method_call,
             std::unique_ptr<flutter::MethodResult<EncodableValue>> result);
 
+        std::unique_ptr<flutter::BasicMessageChannel<EncodableValue>> scanResultMessage;
+        std::unique_ptr<flutter::BasicMessageChannel<EncodableValue>> connectorMessage;
+        std::unique_ptr<flutter::BasicMessageChannel<EncodableValue>> clientMessage;
+
         BluetoothLEAdvertisementWatcher bluetoothLEWatcher{ nullptr };
         event_token bluetoothLEWatcherReceivedToken;
         void BluetoothLEWatcher_Received(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementReceivedEventArgs args);
 
-        std::unique_ptr<flutter::BasicMessageChannel<EncodableValue>> scanResultMessage;
+        BluetoothLEDevice bluetoothLEDevice{ nullptr };
+        fire_and_forget ConnectAsync(uint64_t deviceId);
+        event_token bluetoothLEDeviceConnectionStatusChangedToken;
+        void BluetoothLEDevice_ConnectionStatusChanged(BluetoothLEDevice sender, IInspectable args);
+        void Clean();
+
+        std::map<std::string, GattCharacteristic> gattCharacteristics{};
+        IAsyncOperation<GattCharacteristic> GetCharacteristicAsync(std::string service, std::string characteristic);
+
+        fire_and_forget SetNotifiableAsync(std::string service, std::string characteristic, std::string bleInputProperty);
+
+        fire_and_forget WriteValueAsync(std::string service, std::string characteristic, std::vector<uint8_t> value, std::string bleOutputProperty);
+
+        std::map<std::string, event_token> gattCharacteristicValueChangedTokens{};
+        void GattCharacteristic_ValueChanged(GattCharacteristic sender, GattValueChangedEventArgs args);
     };
 
     // static
@@ -89,6 +148,14 @@ namespace {
             registrar->messenger(),
             "sample_plugin/message.scanResult",
             &flutter::StandardMessageCodec::GetInstance());
+        plugin->connectorMessage = std::make_unique<flutter::BasicMessageChannel<EncodableValue>>(
+            registrar->messenger(),
+            "sample_plugin/message.connector",
+            &flutter::StandardMessageCodec::GetInstance());
+        plugin->clientMessage = std::make_unique<flutter::BasicMessageChannel<EncodableValue>>(
+            registrar->messenger(),
+            "sample_plugin/message.client",
+            &flutter::StandardMessageCodec::GetInstance());
 
         registrar->AddPlugin(std::move(plugin));
     }
@@ -103,9 +170,9 @@ namespace {
         std::unique_ptr<flutter::MethodResult<EncodableValue>> result)
     {
         auto methodName = method_call.method_name();
+        OutputDebugString((L"HandleMethodCall " + to_hstring(methodName) + L"\n").c_str());
         if (methodName == "startScan")
         {
-            OutputDebugString(L"HandleMethodCall startScan\n");
             bluetoothLEWatcher = BluetoothLEAdvertisementWatcher();
             bluetoothLEWatcherReceivedToken = bluetoothLEWatcher.Received({ this, &SamplePlugin::BluetoothLEWatcher_Received });
             bluetoothLEWatcher.Start();
@@ -113,13 +180,54 @@ namespace {
         }
         else if (methodName == "stopScan")
         {
-            OutputDebugString(L"HandleMethodCall stopScan\n");
             if (bluetoothLEWatcher)
             {
                 bluetoothLEWatcher.Stop();
                 bluetoothLEWatcher.Received(bluetoothLEWatcherReceivedToken);
             }
             bluetoothLEWatcher = nullptr;
+            result->Success(nullptr);
+        }
+        else if (methodName == "connect")
+        {
+            auto args = method_call.arguments()->MapValue();
+            auto deviceId = std::stoll(args[EncodableValue("deviceId")].StringValue());
+            ConnectAsync(deviceId);
+            result->Success(nullptr);
+        }
+        else if (methodName == "disconnect")
+        {
+            result->Success(nullptr);
+            connectorMessage->Send(EncodableValue(EncodableMap{
+                {EncodableValue("ConnectionState"), EncodableValue("disconnected")},
+            }));
+            Clean();
+        }
+        else if (methodName == "discoverServices")
+        {
+            result->Success(nullptr);
+            // No need for Windows API
+            connectorMessage->Send(EncodableValue(EncodableMap({
+                {EncodableValue("ServiceState"), EncodableValue("discovered")},
+            })));
+        }
+        else if (methodName == "setNotifiable")
+        {
+            auto args = method_call.arguments()->MapValue();
+            auto service = args[EncodableValue("service")].StringValue();
+            auto characteristic = args[EncodableValue("characteristic")].StringValue();
+            auto bleInputProperty = args[EncodableValue("bleInputProperty")].StringValue();
+            SetNotifiableAsync(service, characteristic, bleInputProperty);
+            result->Success(nullptr);
+        }
+        else if (methodName == "writeValue")
+        {
+            auto args = method_call.arguments()->MapValue();
+            auto service = args[EncodableValue("service")].StringValue();
+            auto characteristic = args[EncodableValue("characteristic")].StringValue();
+            auto value = args[EncodableValue("value")].ByteListValue();
+            auto bleOutputProperty = args[EncodableValue("bleOutputProperty")].StringValue();
+            WriteValueAsync(service, characteristic, value, bleOutputProperty);
             result->Success(nullptr);
         }
         else
@@ -138,9 +246,7 @@ namespace {
         uint8_t* prefix = uint16_t_union{ manufacturerData.CompanyId() }.bytes;
         auto result = std::vector<uint8_t>{ prefix, prefix + sizeof(uint16_t_union) };
 
-        auto reader = DataReader::FromBuffer(manufacturerData.Data());
-        auto data = std::vector<uint8_t>(reader.UnconsumedBufferLength());
-        reader.ReadBytes(data);
+        auto data = to_bytevc(manufacturerData.Data());
         result.insert(result.end(), data.begin(), data.end());
         return result;
     }
@@ -155,6 +261,114 @@ namespace {
             {EncodableValue("manufacturerData"), EncodableValue(manufacturerData)},
             {EncodableValue("rssi"), EncodableValue(args.RawSignalStrengthInDBm())},
         }));
+    }
+
+    fire_and_forget SamplePlugin::ConnectAsync(uint64_t deviceId)
+    {
+        auto device = co_await BluetoothLEDevice::FromBluetoothAddressAsync(deviceId);
+        auto servicesResult = co_await device.GetGattServicesAsync();
+        if (servicesResult.Status() != GattCommunicationStatus::Success)
+        {
+            OutputDebugString((L"GetGattServicesAsync error: " + to_hstring((int32_t) servicesResult.Status()) + L"\n").c_str());
+            connectorMessage->Send(EncodableValue(EncodableMap{
+                {EncodableValue("ConnectionState"), EncodableValue("disconnected")},
+            }));
+            return;
+        }
+        bluetoothLEDevice = device;
+        bluetoothLEDeviceConnectionStatusChangedToken = bluetoothLEDevice.ConnectionStatusChanged({ this, &SamplePlugin::BluetoothLEDevice_ConnectionStatusChanged });
+        connectorMessage->Send(EncodableValue(EncodableMap{
+            {EncodableValue("ConnectionState"), EncodableValue("connected")},
+        }));
+    }
+
+    void SamplePlugin::BluetoothLEDevice_ConnectionStatusChanged(BluetoothLEDevice sender, IInspectable args)
+    {
+        OutputDebugString((L"ConnectionStatusChanged " + to_hstring((int32_t) sender.ConnectionStatus()) + L"\n").c_str());
+        if (sender.ConnectionStatus() == BluetoothConnectionStatus::Disconnected)
+        {
+            connectorMessage->Send(EncodableValue(EncodableMap{
+                {EncodableValue("ConnectionState"), EncodableValue("disconnected")},
+            }));
+            Clean();
+        }
+    }
+
+    void SamplePlugin::Clean()
+    {
+        for (auto it = gattCharacteristics.cbegin(); it != gattCharacteristics.cend(); it = gattCharacteristics.erase(it))
+            it->second.ValueChanged(std::exchange(gattCharacteristicValueChangedTokens[it->first], {}));
+
+        if (bluetoothLEDevice)
+            bluetoothLEDevice.ConnectionStatusChanged(bluetoothLEDeviceConnectionStatusChangedToken);
+        bluetoothLEDevice = nullptr;
+    }
+
+    IAsyncOperation<GattCharacteristic> SamplePlugin::GetCharacteristicAsync(std::string service, std::string characteristic)
+    {
+        // opertator `[]` of colllection requires `CopyConstructible` and `Assignable`
+        if (gattCharacteristics.count(characteristic) == 0)
+        {
+            auto serviceResult = co_await bluetoothLEDevice.GetGattServicesAsync();
+            if (serviceResult.Status() != GattCommunicationStatus::Success)
+                return nullptr;
+            GattDeviceService gattService{ nullptr };
+            for (auto s : serviceResult.Services())
+                if (to_uuidstr(s.Uuid()) == service)
+                    gattService = s;
+
+            auto characteristicResult = co_await gattService.GetCharacteristicsAsync();
+            if (characteristicResult.Status() != GattCommunicationStatus::Success)
+                return nullptr;
+            GattCharacteristic gattCharacteristic{ nullptr };
+            for (auto c : characteristicResult.Characteristics())
+                if (to_uuidstr(c.Uuid()) == characteristic)
+                    gattCharacteristic = c;
+
+            gattCharacteristics.insert(std::make_pair(characteristic, gattCharacteristic));
+        }
+        return gattCharacteristics.at(characteristic);
+    }
+
+    fire_and_forget SamplePlugin::SetNotifiableAsync(std::string service, std::string characteristic, std::string bleInputProperty)
+    {
+        auto gattCharacteristic = co_await GetCharacteristicAsync(service, characteristic);
+        auto descriptorValue = bleInputProperty == "notification" ? GattClientCharacteristicConfigurationDescriptorValue::Notify
+            : bleInputProperty == "indication" ? GattClientCharacteristicConfigurationDescriptorValue::Indicate
+            : GattClientCharacteristicConfigurationDescriptorValue::None;
+        auto writeDescriptorStatus = co_await gattCharacteristic.WriteClientCharacteristicConfigurationDescriptorAsync(descriptorValue);
+        if (writeDescriptorStatus != GattCommunicationStatus::Success)
+            OutputDebugString((L"WriteClientCharacteristicConfigurationDescriptorAsync " + to_hstring((int32_t) writeDescriptorStatus) + L"\n").c_str());
+
+        if (bleInputProperty != "disabled")
+            gattCharacteristicValueChangedTokens[characteristic] = gattCharacteristic.ValueChanged({ this, &SamplePlugin::GattCharacteristic_ValueChanged });
+        else
+            gattCharacteristic.ValueChanged(std::exchange(gattCharacteristicValueChangedTokens[characteristic], {}));
+
+        clientMessage->Send(EncodableValue(EncodableMap({
+            {EncodableValue("characteristicConfig"), EncodableValue(characteristic)},
+        })));
+    }
+
+    fire_and_forget SamplePlugin::WriteValueAsync(std::string service, std::string characteristic, std::vector<uint8_t> value, std::string bleOutputProperty)
+    {
+        auto gattCharacteristic = co_await GetCharacteristicAsync(service, characteristic);
+        auto writeOption = bleOutputProperty == "withoutResponse" ? GattWriteOption::WriteWithoutResponse : GattWriteOption::WriteWithResponse;
+        auto writeValueStatus = co_await gattCharacteristic.WriteValueAsync(from_bytevc(value), writeOption);
+        OutputDebugString((L"WriteValueAsync " + to_hstring(characteristic) + L", " + to_hstring(to_hexstring(value)) + L", " + to_hstring((int32_t) writeValueStatus) + L"\n").c_str());
+    }
+
+    void SamplePlugin::GattCharacteristic_ValueChanged(GattCharacteristic sender, GattValueChangedEventArgs args)
+    {
+        auto uuid = to_uuidstr(sender.Uuid());
+        auto bytes = to_bytevc(args.CharacteristicValue());
+        OutputDebugString((L"GattCharacteristic_ValueChanged " + to_hstring(uuid) + L", " + to_hstring(to_hexstring(bytes)) + L"\n").c_str());
+        clientMessage->Send(EncodableValue(EncodableMap({
+            {EncodableValue("characteristicValue"), EncodableValue(EncodableMap({
+                {EncodableValue("characteristic"), EncodableValue(uuid)},
+                {EncodableValue("value"), EncodableValue(bytes)},
+            }))},
+        })));
     }
 }  // namespace
 
